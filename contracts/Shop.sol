ac86ff932c04b2300352e36458dc0f3c4b52a0df
imcvampire <quocanh96@gmail.com>
2018-10-21 09:45:13 +0700

    feat: 1st run

---
+++ contracts/Shop.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.17;
+
+contract Shop {
+  address[16] public buyers;
+
+  // Adopting a pet
+  function buy(uint clothId) public returns (uint) {
+    require(clothId >= 0 && clothId <= 15);
+
+    buyers[clothId] = msg.sender;
+
+    return clothId;
+  }
+
+  // Retrieving the adopters
+  function getBuyers() public view returns (address[16]) {
+    return buyers;
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,20 @@ App = {
   contracts: {},
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    // Load cloths.
+    $.getJSON('../clothes.json', function(data) {
+      var clothRow = $('#clothRow');
+      var clothTemplate = $('#clothTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        clothTemplate.find('.panel-title').text(data[i].name);
+        clothTemplate.find('img').attr('src', data[i].picture);
+        // clothTemplate.find('.cloth-breed').text(data[i].breed);
+        // clothTemplate.find('.cloth-age').text(data[i].age);
+        // clothTemplate.find('.cloth-location').text(data[i].location);
+        clothTemplate.find('.btn-buy').attr('data-id', data[i].id);
 
-        petsRow.append(petTemplate.html());
+        clothRow.append(clothTemplate.html());
       }
     });
 
@@ -24,39 +24,82 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Shop.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var ShopArtifact = data;
+      App.contracts.Shop = TruffleContract(ShopArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Shop.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted cloths
+      return App.markBuyed();
+    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-buy', App.handleBuy);
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  markBuyed: function(buyers, account) {
+    var shopInstance;
+
+    App.contracts.Shop.deployed().then(function(instance) {
+      shopInstance = instance;
+
+      return shopInstance.getBuyers.call();
+    }).then(function(buyers) {
+      for (i = 0; i < buyers.length; i++) {
+        if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-cloth').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+
   },
 
-  handleAdopt: function(event) {
+  handleBuy: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var clothId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var shopInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Shop.deployed().then(function(instance) {
+        shopInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        return shopInstance.buy(clothId, {from: account});
+      }).then(function(result) {
+        return App.markBuyed();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
