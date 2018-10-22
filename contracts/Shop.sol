bd7560c2bd2858ff2907731828da78530b611022
imcvampire <quocanh96@gmail.com>
2018-10-22 22:10:59 +0700

    fix: typo

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.17;
 contract Shop {
   address[16] public buyers;
 
-  // Adopting a pet
+  // Buy
   function buy(uint clothId) public returns (uint) {
     require(clothId >= 0 && clothId <= 15);
 
